ac38158ef34b47c64d9ec19c911ca70053bd1a7e
Lucas Cerqueira <lucas.cerqueira2@outlook.com>
2022-06-08 21:55:47 -0300

    first commit

---
+++ src/main/java/br/com/lulu/codando/CodandoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lulu.codando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodandoApplication.class, args);
+	}
+
+}
